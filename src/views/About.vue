959d2c8fd007151df54d9d3cd15f04af984bd572
Aaron Haley <15240680+mechdude@users.noreply.github.com>
2018-06-15 07:19:54 -0500

    added start of app using vue-cli

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <img class="logo" src="../assets/logo.svg">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.logo
+  width: 100px;
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.svg">
+    <Hello msg="Welcome to barnub.us!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hello from "@/components/Hello.vue";
+
+export default {
+  name: "home",
+  components: {
+    Hello
+  }
+};
+</script>
